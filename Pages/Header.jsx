1c53fca37c38f4b6e4c83a4677146691c6809cf7
hxncodes <hxncodes@gmail.com>
2023-03-22 18:08:11 +0500

    Menu with Open and Close functionality through states

--- Pages/Header.jsx
+++ Pages/Header.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
-import { AppBar, Toolbar, Typography, Button } from '@mui/material'
+import React, { useState } from 'react'
+import { AppBar, Toolbar, Menu, MenuItem, Typography, Button } from '@mui/material'
 
 function Header() {
+    const [anchorElm, setAnchorElm] = useState(null)
+    const [open, setOpen] = useState(false)
+
+    const handleClose = () => {
+        setAnchorElm(null);
+        setOpen(false)
+    }
+    
+    const handleClick = (e) => {
+        setAnchorElm(e.currentTarget);
+        setOpen(true)
+    }
+
     return (
         <>
             <AppBar variant="elevation">
                 <Toolbar >
                     <Typography variant='h5'>hxnCodes</Typography>
-                    <Button sx={{ marginLeft: 'auto' }} variant='contained' color="error">Login</Button>
+                    <Button
+                        variant="contained"
+                        color="error"
+                        onClick={handleClick}
+                        sx={{ marginLeft: 'auto' }}
+                    >Menu</Button>
+                    <Menu
+                        anchorEl={anchorElm}
+                        open={open}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>Profile</MenuItem>
+                        <MenuItem onClick={handleClose}>Balance</MenuItem>
+                        <MenuItem onClick={handleClose}>Logout</MenuItem>
+                    </Menu>
                 </Toolbar>
             </AppBar>
+
+
         </>
     )
 }
