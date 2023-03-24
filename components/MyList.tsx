fa65a5aa5d02a3de3384a5be3820b89f65f05495
hxncodes <hxncodes@gmail.com>
2023-03-24 12:39:36 +0500

    Expandable list

--- components/MyList.tsx
+++ components/MyList.tsx
@@ -1,24 +1,33 @@
-import React from 'react'
-import { List, Box, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import React, { useState } from 'react'
+import { List, Box, ListItem, ListItemButton, Collapse, ListItemText } from '@mui/material'
 
 const MyList = () => {
 
     const items = ["First", "Second", "Third", "Fourth", "Fifth"]
+    const [open, setOpen] = useState(false)
     return (
         <Box>
             <List sx={{ width: 300 }}>
-
-                {items.map((item) => (
-                    <ListItem divider>
-                        <ListItemButton>
-                            <ListItemText primary={item} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-
-
+                <ListItem divider>
+                    <ListItemButton onClick={() => setOpen(true)}>
+                        <ListItemText primary="Expand List" />
+                    </ListItemButton>
+                </ListItem>
             </List>
-        </Box>
+            <Collapse in={open} >
+                <List sx={{ width: 300, marginLeft: 15 }}>
+                    {items.map((item) => (
+                        <ListItem divider>
+                            <ListItemButton onClick={() => setOpen(false)}>
+                                <ListItemText primary={item} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+
+
+                </List>
+            </Collapse>
+        </Box >
     )
 }
 
