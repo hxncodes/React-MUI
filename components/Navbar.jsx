664b9fb417c57e35344aca6fa9197dafb304935d
Hasnat Rana <hxncodes@gmail.com>
2023-03-25 12:15:08 +0500

    Navbar completed with Custom Components

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,91 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
-import React from "react";
+import styled from "@emotion/styled";
+import { Pets, Mail, Notifications } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Badge,
+  InputBase,
+  Toolbar,
+  Typography,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import React, { useState } from "react";
+
+// Custom components
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+const Search = styled(Box)(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  width: "40%",
+  borderRadius: "4px",
+  // borderRadius: theme.shape.borderRadius,
+}));
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  gap: "20px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: { display: "flex" },
+}));
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: { display: "none" },
+}));
 
 const Navbar = () => {
+  // Hook
+  const [open, setOpen] = useState(false);
+
+  // Functions
+  const handleClose = () => setOpen(false);
   return (
     <AppBar position="sticky">
-      <Toolbar>
-        <Typography variant="h6">hxnCodes</Typography>
-      </Toolbar>
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          hxnCodes
+        </Typography>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={5} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={7} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: "30px", height: "30px" }}
+            src="../assets/images/hxncodes.jpeg"
+            onClick={() => setOpen(true)}
+          />
+        </Icons>
+        <UserBox>
+          <Avatar
+            sx={{ width: "30px", height: "30px" }}
+            src="../../assets/images/hxncodes.jpeg"
+            onClick={() => setOpen(true)}
+          />
+          <Typography variant="span">Wahaj</Typography>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{ verticle: "top", horizontal: "right" }}
+        transformOrigin={{ verticle: "top", horizontal: "right" }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My Account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import Sidebar from "../components/Sidebar";
 import Feed from "../components/Feed";
 import Rightbar from "../components/Rightbar";
 import { Stack } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./theme";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Stack direction="row" spacing={2} justifyContent="space-between">
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar />
+          <Feed />
+          <Rightbar />
+        </Stack>
+      </ThemeProvider>
     </>
   );
 }
